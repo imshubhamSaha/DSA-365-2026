2f4183ffd9974326779b2807783c6380123232cb
Shubham Saha <ssahaa.work@hotmail.com>
2026-02-26 21:19:53 +0530

    Problem of the Day 57

---
+++ Daily-Problem/February-2026/Leetcode/26-02-2026-1404.js
@@ -0,0 +1,23 @@
+// 1404. Number of Steps to Reduce a Number in Binary Representation to One
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numSteps = function (s) {
+  let steps = 0;
+  let carry = 0;
+
+  for (let i = s.length - 1; i > 0; i--) {
+    let bit = s[i] - "0" + carry;
+
+    if (bit === 1) {
+      steps += 2;
+      carry = 1;
+    } else {
+      steps += 1;
+    }
+  }
+
+  return steps + carry;
+};
