c2f0c6abec2a9de2e43d3f7f65b43a6138af730a
Shubham Saha <ssahaa.work@hotmail.com>
2026-03-10 15:55:31 +0530

    Problem of the Day 69

---
+++ Daily-Problem/March-2026/GeeksforGeeks/10-03-2026.py
@@ -0,0 +1,21 @@
+# Subarrays with First Element Minimum
+
+
+class Solution:
+    def countSubarrays(self, arr):
+        n = len(arr)
+        stack = []
+        ans = 0
+        
+        for i in range(n-1, -1, -1):
+            
+            while stack and arr[stack[-1]] >= arr[i]:
+                stack.pop()
+            
+            if not stack:
+                ans += n - i
+            else:
+                ans += stack[-1] - i
+            
+            stack.append(i)
+        return ans
