1cb49be860a5ffa095bbe7e42f718fa3b14c5723
Shubham Saha <ssahaa.work@hotmail.com>
2026-03-27 20:54:42 +0530

    problem of the day 86

---
+++ Daily-Problem/March-2026/Leetcode/27-03-2026-2946.py
@@ -0,0 +1,18 @@
+#2946. Matrix Similarity After Cyclic Shifts
+
+class Solution:
+    def areSimilar(self, mat: List[List[int]], k: int) -> bool:
+        m = len(mat)
+        n = len(mat[0])
+        k = k % n
+
+        for i in range(m) :
+            for j in range(n) :
+                if (i % 2) == 0:
+                    if mat[i][j] != mat[i][(j + k) % n] :
+                        return False
+                else :
+                    if mat[i][j] != mat[i][(j - k + n) % n ] :
+                        return False
+
+        return True
