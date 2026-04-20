0b9101c5b4b6aeac5de21c4ce9ce8e80df3d383a
Shubham Saha <ssahaa.work@hotmail.com>
2026-04-20 22:31:49 +0530

    Problem of the day 110

---
+++ Daily-Problem/APRIL-2026/Geekforgeeks/20-04-2026.py
@@ -0,0 +1,8 @@
+# Count Derangements
+
+
+class Solution:
+    def derangeCount(self, n: int) -> int:
+        if n == 1: return 0
+        if n == 2: return 1
+        return (n-1) * (self.derangeCount(n-1) + self.derangeCount(n-2))
