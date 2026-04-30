7475a72480e7bb707068d5e02d8e065566e42f17
Shubham Saha <ssahaa.work@hotmail.com>
2026-04-30 19:27:09 +0530

    Problem of the day 120

---
+++ Daily-Problem/APRIL-2026/Geekforgeeks/30-04-2026.py
@@ -0,0 +1,18 @@
+# Check if an Array is Max Heap
+
+
+class Solution:
+    def isMaxHeap(self, arr):
+        n = len(arr)
+
+        for i in range(n // 2):
+            left = 2 * i + 1
+            right = 2 * i + 2
+
+            if left < n and arr[i] < arr[left]:
+                return False
+
+            if right < n and arr[i] < arr[right]:
+                return False
+
+        return True
