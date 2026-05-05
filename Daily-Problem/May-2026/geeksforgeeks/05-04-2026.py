b25ad252113aa5d764bb3d1cf1dafcdfb479db2f
Shubham Saha <ssahaa.work@hotmail.com>
2026-05-05 20:38:32 +0530

    Problem of the day 125

---
+++ Daily-Problem/May-2026/geeksforgeeks/05-04-2026.py
@@ -0,0 +1,9 @@
+# Sum of XOR of all pairs
+
+class Solution:
+    def sumXOR(self, arr):
+        n, total = len(arr), 0
+        for i in range(32):
+            count1 = sum((num >> i) & 1 for num in arr)
+            total += count1 * (n - count1) * (1 << i)
+        return total
