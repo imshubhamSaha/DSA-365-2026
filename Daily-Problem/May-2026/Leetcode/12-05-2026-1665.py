5d252aba935757a591e0e34e2bab9f88dad91274
Shubham Saha <ssahaa.work@hotmail.com>
2026-05-12 22:58:13 +0530

    problem of the day 133

---
+++ Daily-Problem/May-2026/Leetcode/12-05-2026-1665.py
@@ -0,0 +1,9 @@
+# 1665. Minimum Initial Energy to Finish Tasks
+
+class Solution:
+    def minimumEffort(self, tasks: List[List[int]]) -> int:
+        tasks.sort(key=lambda x: x[1] - x[0])
+        ans = 0
+        for task in tasks:
+            ans = max(ans + task[0], task[1])
+        return ans
