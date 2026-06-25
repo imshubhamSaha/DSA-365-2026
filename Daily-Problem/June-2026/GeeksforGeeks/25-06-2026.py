ff04f87f2b8bc210d986f7599a410215cf72ade1
Shubham Saha <ssahaa.work@hotmail.com>
2026-06-25 06:34:24 +0530

    Problem of the day 174

---
+++ Daily-Problem/June-2026/GeeksforGeeks/25-06-2026.py
@@ -0,0 +1,20 @@
+# N-Digit Numbers with Increasing Digits
+
+
+class Solution:
+    def solve(self, last, n, c, curr, ans):
+        if n - c > 9 - last:
+            return
+
+        if n - c == 0:
+            ans.append(curr)
+        
+        for i in range(last + 1, 10):
+            self.solve(i, n, c + 1, curr * 10 + i, ans)
+    def increasingNumbers(self, n):
+        ans = []
+        if n == 1:
+            ans.append(0)
+        self.solve(0, n, 0, 0, ans)
+        return ans
+        
