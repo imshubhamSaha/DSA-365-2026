4b5ac499e4f6cdbc6068c7c65c16504fc2f2e343
Shubham Saha <ssahaa.work@hotmail.com>
2026-06-27 16:51:10 +0530

    Problem of the day 178

---
+++ Daily-Problem/June-2026/GeeksforGeeks/27-06-2026.py
@@ -0,0 +1,18 @@
+# Ways to Tile the Floor
+
+class Solution:
+	def countWays(self, n, m):
+		MOD = 10**9 + 7
+
+        dp = [0] * (n + 1)
+
+        for i in range(n + 1):
+            if i < m:
+                dp[i] = 1
+            elif i == m:
+                dp[i] = 2
+            else:
+                dp[i] = (dp[i - 1] + dp[i - m]) % MOD
+
+        return dp[n]
+		
