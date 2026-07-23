ec698c68935744d46dc98ee929533747a7bc4972
Shubham Saha <ssahaa.work@hotmail.com>
2026-07-23 13:56:49 +0530

    Problem of the day 204

---
+++ Daily-Problem/JULY-2026/geeksforgeeks/23-07-2026.py
@@ -0,0 +1,17 @@
+# Check Preorder of BST
+class Solution:
+    def canRepresentBST(self, arr):
+        stack = []
+        root = float('-inf')
+
+        for x in arr:
+            if x < root:
+                return 0
+
+            while stack and stack[-1] < x:
+                root = stack.pop()
+
+            stack.append(x)
+
+        return 1
+        
