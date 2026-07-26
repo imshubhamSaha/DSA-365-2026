c03fa364001438d81a37d8e017f40f36e16020bc
Shubham Saha <ssahaa.work@hotmail.com>
2026-07-26 16:16:12 +0530

    Problem of the day 207

---
+++ Daily-Problem/JULY-2026/geeksforgeeks/26-07-2026.py
@@ -0,0 +1,17 @@
+# Complete Binary Tree Traversal with Array Input
+class Solution:
+    def levelSort(self, arr):
+        res = []
+        i = 0
+        level_size = 1
+        n = len(arr)
+
+        while i < n:
+            level = arr[i:min(i + level_size, n)]
+            level.sort()
+            res.append(level)
+            i += level_size
+            level_size *= 2
+
+        return res
+        
