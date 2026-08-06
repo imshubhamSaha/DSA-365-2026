111520d0517574f1232e8c806d27f636ef3a6aa6
Shubham Saha <ssahaa.work@hotmail.com>
2026-08-06 17:32:55 +0530

    Problem of the day 218

---
+++ Daily-Problem/AUGUST-2026/Leetcode/06-08-2026-3345.py
@@ -0,0 +1,10 @@
+# 3345. Smallest Divisible Digit Product I
+class Solution:
+    def smallestNumber(self, n: int, t: int) -> int:
+        q, r = divmod(n, 10)
+        
+        req = t // gcd(max(q, 1), t)
+        nxt = ((r + req - 1) // req) * req
+        x = nxt - (nxt - 10) * (nxt // 10)
+
+        return q * 10 + x
