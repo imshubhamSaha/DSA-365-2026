4eb77cc851d640488b3baa888c72792b74e746ff
Shubham Saha <ssahaa.work@hotmail.com>
2026-08-10 13:09:21 +0530

    Problem of the day 222

---
+++ Daily-Problem/AUGUST-2026/Leetcode/10-08-2026-1510.py
@@ -0,0 +1,19 @@
+# 1510. Stone Game IV
+
+class Solution:
+    def winnerSquareGame(self, n: int) -> bool:
+        dp = [False] * (n + 1)
+
+        dp[0] = False
+
+        for i in range(1, n + 1):
+            j = 1
+            while j * j <= i:
+                if not dp[i - j * j]:
+                    dp[i] = True
+
+                    break
+
+                j += 1
+
+        return dp[n]
