c908732452ad5a8af1d9280a22a735b80856f608
Shubham Saha <ssahaa.work@hotmail.com>
2026-08-21 14:50:32 +0530

    Problem of the day 233

---
+++ Daily-Problem/AUGUST-2026/Geeksforgeeks/21-08-2026.py
@@ -0,0 +1,13 @@
+# Transform String
+class Solution:
+    def transform(self, s1, s2): 
+        #code here
+        from collections import Counter
+        if Counter(s1) != Counter(s2):
+            return - 1
+        n = len(s1)
+        j = n - 1
+        for i in reversed(range(n)):
+            if s1[i] == s2[j]:
+                j -= 1
+        return j + 1
