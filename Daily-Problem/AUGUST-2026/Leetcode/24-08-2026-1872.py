73c6a871df983a45d7fe61a17a02409cb48901d9
Shubham Saha <ssahaa.work@hotmail.com>
2026-08-24 16:32:18 +0530

    Problem of the day 236

---
+++ Daily-Problem/AUGUST-2026/Leetcode/24-08-2026-1872.py
@@ -0,0 +1,14 @@
+# 1872. Stone Game VIII
+class Solution:
+    def stoneGameVIII(self, stones: List[int]) -> int:
+        n = len(stones)
+
+        prefix = stones[:]
+        for i in range(1, n):
+            prefix[i] += prefix[i - 1]
+
+        best = prefix[n - 1]
+        for i in range(n - 2, 0, -1):
+            best = max(best, prefix[i] - best)
+
+        return best
