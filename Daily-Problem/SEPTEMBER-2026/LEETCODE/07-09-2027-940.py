12a16f0ae15107ec795dd414f857f5189e496625
Shubham Saha <ssahaa.work@hotmail.com>
2026-09-07 15:21:59 +0530

    Problem of the day 250

---
+++ Daily-Problem/SEPTEMBER-2026/LEETCODE/07-09-2027-940.py
@@ -0,0 +1,18 @@
+# 940. Distinct Subsequences II
+class Solution:
+    def distinctSubseqII(self, s: str) -> int:
+        n = len(s)
+        MOD = 10**9 + 7
+
+        dp = [1] * n
+        result = 0
+
+        for i in range(n):
+
+            for j in range(i):
+                if s[i] != s[j]:
+                    dp[i] = (dp[i] + dp[j]) % MOD
+
+            result = (result + dp[i]) % MOD
+
+        return result
