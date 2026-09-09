324ae281d92c0028370a0f4a34de84dab88824bb
Shubham Saha <ssahaa.work@hotmail.com>
2026-09-09 15:40:44 +0530

    Problem of the day 252

---
+++ Daily-Problem/SEPTEMBER-2026/LEETCODE/09-09-2026-3871.py
@@ -0,0 +1,24 @@
+# 3871. Count Commas in Range II
+
+
+class Solution:
+    def countCommas(self, n: int) -> int:
+        if n <= 999:
+            return 0
+
+        totalCommas = 0
+        rangeStart = 1000
+        rangeEnd = rangeStart * 1000 - 1
+        commas = 1
+
+        while rangeStart <= n:
+            numbers = min(n, rangeEnd) - rangeStart + 1
+            totalCommas += commas * numbers
+
+            if rangeEnd > n:
+                break
+            rangeStart *= 1000
+            rangeEnd = rangeStart * 1000 - 1
+            commas += 1
+
+        return totalCommas
