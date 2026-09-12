12678e76ce73c538f207b22902ea993ced21edc6
Shubham Saha <ssahaa.work@hotmail.com>
2026-09-12 17:31:01 +0530

    Problem of the day 256

---
+++ Daily-Problem/SEPTEMBER-2026/GEEKSFORGEEKS/12-09-2026.py
@@ -0,0 +1,16 @@
+# Max Product Subsequence of Size K
+class Solution:
+    def maxProduct(self, arr: list[int], k: int) -> int:
+        # code here
+        arr.sort()
+        ans=float('-inf')
+        n=len(arr)
+        for left in range(0,k+1,2):
+            right=k-left
+            prod=1
+            for i in range(left):
+                prod*=arr[i]
+            for i in range(n-right,n,1):
+                prod*=arr[i]
+            ans=max(prod,ans)
+        return ans
